b606763d08617e477e999234205e4ae7d9df2d71
Mabel I.M. <ma.b.el.202423@gmail.com>
2024-07-11 22:15:44 -0700

    Setting up react app and added some component folders.

---
+++ prospera-project/src/Components/AboutPage/AboutPage.jsx
@@ -0,0 +1,17 @@
+import './AboutPage.css'
+
+const AboutPage = () => {
+    return (
+        <>
+            <div className='aboutHeader'>
+                <h1>The Path to Prosper</h1>
+
+                <div className='rightHeaderBody'>
+                    <p>Managing finances can be challenging, especially for young adults transitioning into independence and immigrants adapting to a new financial landscape. Our web app aims to simplify and streamline personal finance management, providing users with the resources and guidance they need to achieve financial stability and growth.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AboutPage
