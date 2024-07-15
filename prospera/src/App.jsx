2f31f6851226f5d6035f20ccf0ae2c9266a3466c
Mabel I.M. <ma.b.el.202423@gmail.com>
2024-07-15 09:53:28 -0700

    Got started on the footer and about page.

--- prospera/src/App.jsx
+++ prospera/src/App.jsx
@@ -1,15 +1,18 @@
 // import { useState } from 'react'
 import './App.css'
 import AboutPage from './Components/AboutPage/AboutPage'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
   return (
     <>
+      <AboutPage />
       <div>
-        <AboutPage />
         
       </div>
+
+      <Footer />
     </>
   )
 }
