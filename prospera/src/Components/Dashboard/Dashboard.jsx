a23306c7419b07ba5a4535fbb8d23c79e9230610
kaileychavez165 <kachavez165@gmail.com>
2024-07-19 12:03:55 -0700

    Create dashboard w/ basic functionality (adding, deleting, drag/drop)

--- prospera/src/Components/Dashboard/Dashboard.jsx
+++ prospera/src/Components/Dashboard/Dashboard.jsx
@@ -1,139 +1,141 @@
-import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
 import './Dashboard.css'
 
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
+// Makes layout responsive
+const DashboardLayout = WidthProvider(Responsive);
 
 const Dashboard = () => {
-    const [layouts, setLayouts] = useState(null);
+  // Layouts for every screen size, makes dashboard responsive
+  const [layouts, setLayouts] = useState({ 
+    lg: [],
+    md: [],
+    sm: [],
+    xs: [],
+    xxs: []
+  });
+
+  // Widgets for testing purposes
   const [widgetArray, setWidgetArray] = useState([
-    { i: "widget1", x: 0, y: 0, w: 2, h: 2 },
-    { i: "widget2", x: 2, y: 2, w: 2, h: 2 },
-    { i: "widget3", x: 4, y: 4, w: 2, h: 2 },
+    { i: "Widget 1", x: 0, y: 0, w: 2, h: 2, color: "#FFB6C1" },
+    { i: "Widget 2", x: 2, y: 2, w: 2, h: 2, color: "#ADD8E6" },
+    { i: "Widget 3", x: 4, y: 4, w: 2, h: 2, color: "#90EE90" },
   ]);
 
-  const handleModify = (layouts, layout) => {
-    const tempArray = widgetArray;
-    setLayouts(layout);
-    layouts?.map((position) => {
-      tempArray[Number(position.i)].x = position.x;
-      tempArray[Number(position.i)].y = position.y;
-      tempArray[Number(position.i)].width = position.w;
-      tempArray[Number(position.i)].height = position.h;
+  // Used to handle widget modifications (position/size) and updating layouts
+  // Layouts and allLayouts passed in as arguments by react-grid-layout
+  const handleModify = (layout, allLayouts) => {
+    const updatedWidgets = widgetArray.map((widget) => {
+      const updatedWidget = layout.find((obj) => obj.i === widget.i);
+      if (updatedWidget) {
+        return {
+          ...widget,
+          x: updatedWidget.x,
+          y: updatedWidget.y,
+          w: updatedWidget.w,
+          h: updatedWidget.h,
+        };
+      } else {
+        return widget;
+      }
     });
-    setWidgetArray(tempArray);
+    setLayouts(allLayouts);
+    setWidgetArray(updatedWidgets);
   };
 
+  // Used to handle adding widgets and updating layouts
   const handleAdd = () => {
-    setWidgetArray([
-      ...widgetArray,
-      { i: "widget" + (widgetArray.length + 1), x: 0, y: 0, w: 2, h: 2 },
-    ]);
+    const colors = ["#FFB6C1", "#ADD8E6", "#90EE90", "#FFD700", "#FFA07A"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    const newWidget = {
+      i: `Widget ${widgetArray.length + 1}`,
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 2,
+      color: randomColor,
+    };
+
+    setWidgetArray([...widgetArray, newWidget]);
+    setLayouts((prevLayouts) => ({
+      ...prevLayouts,
+      lg: [...prevLayouts.lg, newWidget],
+      md: [...prevLayouts.md, newWidget],
+      sm: [...prevLayouts.sm, newWidget],
+      xs: [...prevLayouts.xs, newWidget],
+      xxs: [...prevLayouts.xxs, newWidget],
+    }));
   };
 
+  // Used to handle deleting widgets and updating layouts
   const handleDelete = (key) => {
-    const tempArray = widgetArray.slice();
-    const index = tempArray.indexOf(tempArray.find((data) => data.i === key));
-    tempArray.splice(index, 1);
-    setWidgetArray(tempArray);
+    const updatedWidgets = widgetArray.filter((widget) => widget.i !== key);
+    setWidgetArray(updatedWidgets);
+    setLayouts((prevLayouts) => ({
+      ...prevLayouts,
+      lg: prevLayouts.lg.filter((layout) => layout.i !== key),
+      md: prevLayouts.md.filter((layout) => layout.i !== key),
+      sm: prevLayouts.sm.filter((layout) => layout.i !== key),
+      xs: prevLayouts.xs.filter((layout) => layout.i !== key),
+      xxs: prevLayouts.xxs.filter((layout) => layout.i !== key),
+    }));
+  };
+
+  const stopPropagation = (evt) => {
+    evt.stopPropagation();
   };
 
   return (
-    <div>
-      <button onClick={() => handleAdd()}>Add Widget</button>
+    <div className='dashboardBody'>
+      <button onClick={handleAdd}>Add Widget</button>
 
-      <ResponsiveReactGridLayout
+      <DashboardLayout
+        className='dash'
         onLayoutChange={handleModify}
         verticalCompact={true}
-        layout={layouts}
+        layouts={layouts}
+        // Breakpoints and cols are used for responsive design
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        preventCollision={false}
         cols={{ lg: 8, md: 8, sm: 4, xs: 2, xxs: 2 }}
         autoSize={true}
-        margin={{
-          lg: [20, 20],
-          md: [20, 20],
-          sm: [20, 20],
-          xs: [20, 20],
-          xxs: [20, 20],
-        }}
+        margin={[20, 20]}
       >
-        {widgetArray?.map((widget, index) => {
-          return (
-            <div
-              className="reactGridItem"
-              key={index}
-              data-grid={{
-                x: widget?.x,
-                y: widget?.y,
-                w: widget?.w,
-                h: widget?.h,
-                i: widget.i,
-                minW: 2,
-                maxW: Infinity,
-                minH: 2,
-                maxH: Infinity,
-                isDraggable: true,
-                isResizable: true,
+        {widgetArray.map((widget) => (
+          <div
+            // Widget container
+            key={widget.i}
+            className="reactGridItem"
+            data-grid={{
+              x: widget.x,
+              y: widget.y,
+              w: widget.w,
+              h: widget.h,
+              i: widget.i,
+            }}
+            style={{ backgroundColor: widget.color }}
+          >
+            <button
+              // Prevent dragging when trying to delete widget
+              onMouseDown={stopPropagation}
+              onTouchStart={stopPropagation}
+              className="deleteButton no-drag"
+              onClick={() => {
+                handleDelete(widget.i);
               }}
             >
-              <button
-                className="deleteButton"
-                onClick={() => handleDelete(widget.i)}
-              >
-                x
-              </button>
-              <div>{widget.i}</div>
+              x
+            </button>
+            <div // Widget content
+            >
+            {widget.i}
             </div>
-          );
-        })}
-      </ResponsiveReactGridLayout>
+          </div>
+        ))}
+      </DashboardLayout>
     </div>
   );
 };
 
-export default Dashboard
-
-
-        // <div>
-        //     <div className="welcomeBack">
-        //         <h2>Welcome back, ____!</h2>
-        //     </div>
-
-        //     <div className="dashboard">
-        //         <div className='left-column'>
-        //             <div className='area2'>
-        //                 <div className='area1'> 
-        //                     <img className='image1' src="https://placehold.co/300x200" alt="placeholder" />
-        //                     <div className='area0'>
-        //                         <h3>Header1</h3>
-        //                         <h3>Header1</h3>
-        //                     </div>
-        //                 </div>
-        //                 <div className='dashboardBankAccts'>
-        //                     <h3>Checkings</h3>
-        //                     <h3>Savings</h3>
-        //                 </div>
-                        
-        //             </div>
-
-        //             <img className='image2 right-column' src="https://preview.redd.it/can-they-please-bring-back-the-old-stocks-widget-v0-tu2me20n0cwb1.jpg?width=1284&format=pjpg&auto=webp&s=217d0fd8abcbfa6eade90e24c873b517e6b583bd" alt="placeholder" />
-        //         </div>
-
-        //         <div className='area4'>
-        //             <img className='image3' src="https://placehold.co/200x150" alt="placeholder" />
-        //             <img className='image3' src="https://placehold.co/200x150" alt="placeholder" />
-        //             <img className='image3' src="https://placehold.co/200x150" alt="placeholder" />
-        //             <img className='image3' src="https://placehold.co/200x150" alt="placeholder" />
-        //         </div>
-
-        //         <Link to={`/api/chat`}>
-        //             <div className='chatIcon'>
-        //                 <img src="https://placehold.co/50x50" alt="chat icon" />
-        //             </div>
-        //         </Link>
-        //     </div>
-        // </div>      
+export default Dashboard;
