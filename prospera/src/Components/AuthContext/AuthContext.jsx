5486cbb7df483a0b6d8a860d57454c7bd8b56356
Mabel I.M. <ma.b.el.202423@gmail.com>
2024-07-19 09:29:17 -0700

    changes to format

--- prospera/src/Components/AuthContext/AuthContext.jsx
+++ prospera/src/Components/AuthContext/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useContext } from 'react'
+import PropTypes from 'prop-types';
 
 const AuthContext = createContext(null);
 
@@ -19,4 +20,8 @@ export const useAuth = () => {
         throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.string.isRequired,
 };
