d1d87a512a06786b22fb0e2093b03b7ba686788c
Nathan Quiroa <nquiroa@nquiroa-ltmzqp6.internal.salesforce.com>
2024-07-23 14:37:04 -0700

    integreated the topic selection feature prompting users not only the option to submit or skip various topics they could be interested in, but also a recommended news tab related to those topics

--- prospera/src/App.jsx
+++ prospera/src/App.jsx
@@ -1,44 +1,44 @@
+// App.jsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-// import axios from "axios";
+import './App.css';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Header from './Components/Header/Header';
-import LoginPage from './Components/LoginPage/LoginPage'
+import LoginPage from './Components/LoginPage/LoginPage';
 import Dashboard from './Components/Dashboard/Dashboard';
-import RegisterPage from './Components/RegisterPage/RegisterPage'
-import NewsFeed from './Components/NewsFeed/NewsFeed'
-import ForgotPasswordPage from './Components/ForgotPasswordPage/ForgotPasswordPage'
-import Footer from './Components/Footer/Footer'
+import RegisterPage from './Components/RegisterPage/RegisterPage';
+import NewsFeed from './Components/NewsFeed/NewsFeed';
+import ForgotPasswordPage from './Components/ForgotPasswordPage/ForgotPasswordPage';
+import Footer from './Components/Footer/Footer';
 import NewsCardDetail from './Components/NewsCardDetail/NewsCardDetail';
-// import {ScrollContainer} from 'react-scroll-motion';
 import ChatbotPage from './Components/ChatbotPage/ChatbotPage';
+import TopicSelectionPage from './Components/TopicSelectionPage/TopicSelectionPage';
 import { AuthProvider } from './Components/AuthContext/AuthContext';
 import LogOut from './Components/LogOut/LogOut';
 
 function App() {
-
   return (
     <div className='app'>
       <Router>
         <AuthProvider>
-            <Header />
-              <Routes>
-                <Route path="/:section?" element={<LandingPage/>}/>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/logout" element={<LogOut />} />
-                <Route path="/forgot" element={<ForgotPasswordPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/news" element={<NewsFeed />} />
-                <Route path="/news/:articleId" element={<NewsCardDetail />} />
-                <Route path="/chat" element={<ChatbotPage />} />
-                <Route path="*" element={<h1>Not Found</h1>} />
-              </Routes>
-            <Footer  className="footer"/>
+          <Header />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" element={<LogOut />} />
+            <Route path="/forgot" element={<ForgotPasswordPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/news" element={<NewsFeed />} />
+            <Route path="/news/:articleId" element={<NewsCardDetail />} />
+            <Route path="/chat" element={<ChatbotPage />} />
+            <Route path="/topic-selection" element={<TopicSelectionPage />} />
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+          <Footer className="footer" />
         </AuthProvider>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
