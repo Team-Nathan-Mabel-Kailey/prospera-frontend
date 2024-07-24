75def0da586647afffda5079291f991d63066ef2
Nathan Quiroa <nquiroa@nquiroa-ltmzqp6.internal.salesforce.com>
2024-07-24 14:10:14 -0700

    merging app.jsx

--- prospera/src/App.jsx
+++ prospera/src/App.jsx
@@ -1,41 +1,34 @@
-// App.jsx
+// import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import './App.css'
+// import axios from "axios";
 import LandingPage from './Components/LandingPage/LandingPage';
 import Header from './Components/Header/Header';
-import LoginPage from './Components/LoginPage/LoginPage';
+import LoginPage from './Components/LoginPage/LoginPage'
 import Dashboard from './Components/Dashboard/Dashboard';
-import RegisterPage from './Components/RegisterPage/RegisterPage';
-import NewsFeed from './Components/NewsFeed/NewsFeed';
-import ForgotPasswordPage from './Components/ForgotPasswordPage/ForgotPasswordPage';
-import Footer from './Components/Footer/Footer';
+import RegisterPage from './Components/RegisterPage/RegisterPage'
+import NewsFeed from './Components/NewsFeed/NewsFeed'
+import ForgotPasswordPage from './Components/ForgotPasswordPage/ForgotPasswordPage'
+import Footer from './Components/Footer/Footer'
 import NewsCardDetail from './Components/NewsCardDetail/NewsCardDetail';
+// import {ScrollContainer} from 'react-scroll-motion';
 import ChatbotPage from './Components/ChatbotPage/ChatbotPage';
-import TopicSelectionPage from './Components/TopicSelectionPage/TopicSelectionPage';
 import { AuthProvider } from './Components/AuthContext/AuthContext';
-import LogOut from './Components/LogOut/LogOut';
-import { useState } from 'react';
-import HamMenu from './Components/HamMenu/HamMenu';
 
 function App() {
-  const [open, setOpen] = useState(false);
-  
+
   return (
-    <div className='app'>
-      <Router>
+    <>
         <AuthProvider>
-            <Header 
-              open= {open}
-              setOpen={setOpen}
-            />
-            <HamMenu
-              open={open}
-            />
             
+            <Router>
+            <Header />
+
               <Routes>
-                <Route path="/:section?" element={<LandingPage/>}/>
+                <Route path="/" element={<LandingPage/>}/>
+                <Route path="/about" element={<LandingPage scrollTo="aboutPage" />} />
+                <Route path="/contact" element={<LandingPage scrollTo="contactPage" />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/logout" element={<LogOut />} />
                 <Route path="/forgot" element={<ForgotPasswordPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/dashboard" element={<Dashboard />} />
@@ -44,12 +37,12 @@ function App() {
                 <Route path="/chat" element={<ChatbotPage />} />
                 <Route path="*" element={<h1>Not Found</h1>} />
               </Routes>
-              <div className='footerSpace'></div>
-            <Footer  className="footer"/>
+            </Router>
+
+            <Footer />
         </AuthProvider>
-      </Router>
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
