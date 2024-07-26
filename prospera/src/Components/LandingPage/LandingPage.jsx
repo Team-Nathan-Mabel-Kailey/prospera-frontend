f53e2d93be0dcbdedcb19f5d1ff2f184aaf6fd0d
Mabel I.M. <ma.b.el.202423@gmail.com>
2024-07-25 19:19:51 -0700

    landing page cards stats

--- prospera/src/Components/LandingPage/LandingPage.jsx
+++ prospera/src/Components/LandingPage/LandingPage.jsx
@@ -50,8 +50,8 @@ const LandingPage = () => {
                             <figure>
                                 <img src="https://placehold.jp/400x200.png" alt="" />
                             </figure>
-                        <h3>Placeholder</h3>
-                        <Link to={"/"} target="_blank" className="read-more">Read more
+                        <h4>&quot;A recent WalletHub survey found that more than one-quarter of Gen Z admits to lacking confidence in their financial knowledge&quot;</h4>
+                        <Link to={"https://wallethub.com/blog/generational-finances-survey/133122"} target="_blank" className="read-more">Read more
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
                             </svg>
